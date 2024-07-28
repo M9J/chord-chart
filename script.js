5c4316ac4a117221d19f0b4d34f02d60adda883e
Martian Jeirgif <mgj.exe@gmail.com>
2024-07-28 20:49:03 +0530

    template loading fixes

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const baseURL = location.href;
       const chordScaleTemplate = await buildChordTemplates(key, chords);
       const indexContent = document.getElementById("index-content");
       indexContent.appendChild(chordScaleTemplate);
+      loadChordTemplates(key, chords);
     }
   }
 })();
@@ -20,36 +21,33 @@ async function buildChordTemplates(key, chords) {
   if (!chords) throw new Error("chords not provided");
   const chordList1 = document.createElement("div");
   chordList1.classList.add("chord-list");
-  const chordListName1 = document.createElement("div");
-  chordListName1.classList.add("chord-list-name");
-  chordListName1.innerHTML = `${key} key`;
   const chordListPreview1 = document.createElement("div");
   chordListPreview1.classList.add("chord-list-preview");
   const previewContainer1 = document.createElement("div");
   previewContainer1.classList.add("preview-container");
   const previewWindow1 = document.createElement("div");
-  previewWindow1.id = "preview-window";
-  const chordTemplates = await loadChordTemplates(key, chords);
-  if (chordTemplates.length > 0) {
-    previewWindow1.innerHTML = chordTemplates.join("");
-    previewContainer1.appendChild(previewWindow1);
-    chordListPreview1.appendChild(previewContainer1);
-    // chordList1.appendChild(chordListName1);
-    chordList1.appendChild(chordListPreview1);
-  }
+  previewWindow1.classList.add("preview-window");
+  previewWindow1.id = "preview-window_" + key;
+  previewContainer1.appendChild(previewWindow1);
+  chordListPreview1.appendChild(previewContainer1);
+  chordList1.appendChild(chordListPreview1);
   return chordList1;
 }
 
 async function loadChordTemplates(key, chords) {
   if (!key) throw new Error("key not provided");
   if (!chords) throw new Error("chords not provided");
-  const chordHTMLs = [];
   for (let chord of chords) {
     const chordPath = `${baseURL}chord-chart/${key}/${chord}.html`;
-    const chordFile = await fetch(chordPath);
-    if (!chordFile) throw new Error("chordFile not found");
-    const chordHTML = await chordFile.text();
-    if (chordHTML) chordHTMLs.push(chordHTML);
+    if (chordPath) loadChord(chordPath, key);
   }
-  return chordHTMLs;
+}
+
+async function loadChord(chordPath, key) {
+  if (!chordPath) throw new Error("chordPath not provided");
+  if (!key) throw new Error("key not provided");
+  const chordFile = await fetch(chordPath);
+  const chordHTML = await chordFile.text();
+  const previewWindow1 = document.getElementById("preview-window_" + key);
+  previewWindow1.innerHTML = previewWindow1.innerHTML + chordHTML;
 }
